825c9448d238548e8f6cf1f5ce172cc7421d5935
tsanius <128583702+tsanius@users.noreply.github.com>
2023-10-29 16:29:31 +0100

    Update UI.tsx

--- packages/app/src/components/ui/UI.tsx
+++ packages/app/src/components/ui/UI.tsx
@@ -67,7 +67,7 @@ export function UI({ fileInputProps }: { fileInputProps: DropzoneInputProps }) {
         htmlFor="file-input"
         className="cursor-pointer flex m-2 py-2 px-4 text-sm font-medium text-white bg-gray-700 hover:bg-gray-600 border border-gray-600 rounded-lg shadow-sm"
       >
-        Select File
+        Select URL
         <input id="file-input" {...fileInputProps} />
       </label>
 
